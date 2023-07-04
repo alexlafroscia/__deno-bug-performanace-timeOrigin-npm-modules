5ebd209e6777fda40daafedd69828308527fe983
Alex LaFroscia <alex@lafroscia.com>
2023-07-04 00:33:24 -0400

    add a failing test

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,11 @@
 import { assertEquals } from "https://deno.land/std@0.192.0/testing/asserts.ts";
-import { add } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+import * as Effect from "npm:@effect/io/Effect";
+
+Deno.test("running an Effect", async () => {
+  const result = await Effect.runPromise(
+    Effect.promise(() => Promise.resolve("success"))
+  );
+
+  assertEquals(result, "success");
 });
